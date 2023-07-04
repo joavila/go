e289a04c659c48a064a8b85d2b1363fec0bea2e1
Jorge Avila A <jorge.avila@gmail.com>
2023-07-04 14:08:08 +0000

    Adds two contexts

--- tmp.0bIHeuvHpY/main.go
+++ tmp.0bIHeuvHpY/main.go
@@ -7,8 +7,13 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Healthcheck invoked from: %s", r.RemoteAddr )
+        fmt.Fprint(w, "OK")
+    })
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Invoked from: %s", r.RemoteAddr)
         fmt.Fprint(w, "hello world\n")
     })
-    log.Fatal(http.ListenAndServe("localhost:7777", nil))
+    log.Fatal(http.ListenAndServe(":80", nil))
 }
